02f8658b11f56f4de6f71866fb44e69a46d3af4c
fiercehut <fiercehut@dynv6.net>
2025-07-10 14:05:49 +0200

    Simplify training logic

--- nn/src/main/java/de/hexgame/nn/Trainer.java
+++ nn/src/main/java/de/hexgame/nn/Trainer.java
@@ -5,91 +5,44 @@ import de.hexgame.logic.GameState;
 import de.hexgame.logic.Player;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
-import org.deeplearning4j.datasets.iterator.file.FileMultiDataSetIterator;
 import org.nd4j.linalg.dataset.MultiDataSet;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 @Slf4j
 public class Trainer implements Runnable {
     private static final int GAME_COUNT = 100;
-    private static final Path DATA_DIRECTORY = Path.of("training-data");
-    private static final Pattern MODEL_PATTERN = Pattern.compile("model-(\\d+).zip");
-    private static final Pattern DATA_SET_PATTERN = Pattern.compile("dataset-(\\d+)");
-
-    static {
-        try {
-            Files.createDirectories(DATA_DIRECTORY);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static final Path MODEL_PATH = Path.of("model.zip");
 
     private final List<MultiDataSet> dataSetList = new ArrayList<>(2000);
     private final AtomicInteger gameCounter = new AtomicInteger(0);
     private Model model;
-    private int modelNumber = 0;
-    private int dataSetNumber = 0;
-    private Path dataSetPath;
 
     @SneakyThrows
     @Override
     public void run() {
-        try (Stream<Path> paths = Files.list(DATA_DIRECTORY)) {
-            paths.forEach(file -> {
-                String fileName = file.getFileName().toString();
-                Matcher modelMatcher = MODEL_PATTERN.matcher(fileName);
-                if (modelMatcher.matches()) {
-                    int savedModelNumber = Integer.parseInt(modelMatcher.group(1));
-                    if (savedModelNumber > modelNumber) {
-                        modelNumber = savedModelNumber;
-                    }
-                }
-                Matcher dataSetMatcher = DATA_SET_PATTERN.matcher(fileName);
-                if (dataSetMatcher.matches() && FileUtils.listFiles(file.toFile(), null, false).size() >= 10) {
-                    int savedDataSetNumber = Integer.parseInt(dataSetMatcher.group(1));
-                    if (savedDataSetNumber > dataSetNumber) {
-                        dataSetNumber = savedDataSetNumber;
-                    }
-                }
-            });
-        }
-        if (modelNumber == 0) {
-            model = new Model();
+        if (Files.exists(MODEL_PATH)) {
+            model = new Model(MODEL_PATH.toFile(), true);
         } else {
-            model = new Model(DATA_DIRECTORY.resolve(String.format("model-%d.zip", modelNumber)).toFile(), true);
-        }
-        if (dataSetNumber != 0) {
-            dataSetPath = DATA_DIRECTORY.resolve(String.format("dataset-%d", dataSetNumber));
+            model = new Model();
         }
         model.start();
-        while (dataSetNumber < 10) {
-            if (dataSetNumber <= modelNumber) {
-                dataSetNumber++;
-                dataSetPath = Files.createDirectories(DATA_DIRECTORY.resolve(String.format("dataset-%d", dataSetNumber)));
-                log.info("Generating training data {}...", dataSetNumber);
-                generateDataSet();
-            }
-            modelNumber++;
-            log.info("Fitting the model {}...", modelNumber);
+        while (true) {
+            log.info("Generating training data...");
+            generateDataSet();
+            log.info("Fitting the model...");
             fitModel();
-            model.save(DATA_DIRECTORY.resolve(String.format("model-%d.zip", modelNumber)).toFile());
+            model.save(MODEL_PATH.toFile());
         }
     }
 
     @SneakyThrows
     private void generateDataSet() {
-        gameCounter.set(FileUtils.listFiles(dataSetPath.toFile(), null, false).size() * 10);
+        gameCounter.set(0);
         final int threadCount = Runtime.getRuntime().availableProcessors();
         Thread[] threads = new Thread[threadCount];
         for (int i = 0; i < threadCount; i++) {
@@ -99,16 +52,10 @@ public class Trainer implements Runnable {
         for (int i = 0; i < threadCount; i++) {
             threads[i].join();
         }
-        synchronized (this) {
-            if (!dataSetList.isEmpty()) {
-                MultiDataSet.merge(dataSetList).save(dataSetPath.resolve(UUID.randomUUID() + ".bin").toFile());
-                dataSetList.clear();
-            }
-        }
     }
 
     private void fitModel() {
-        model.fit(new FileMultiDataSetIterator(dataSetPath.toFile(), 128), 5);
+        model.fit(MultiDataSet.merge(dataSetList), 3);
     }
 
     @SneakyThrows
@@ -133,12 +80,6 @@ public class Trainer implements Runnable {
             gameDataB.clear();
             gameState.reset();
             log.info("Game {} has finished.", gameNumber);
-            synchronized (this) {
-                if (dataSetList.size() >= 1000) {
-                    MultiDataSet.merge(dataSetList).save(dataSetPath.resolve(UUID.randomUUID() + ".bin").toFile());
-                    dataSetList.clear();
-                }
-            }
         }
     }
 }
